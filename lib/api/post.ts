388d4adb0c87899e7b3b5f8cbac3ec78dbb908e5
npratik0 <neupanepratik00@gmail.com>
2026-03-05 16:39:38 +0545

    Post API added

---
+++ lib/api/post.ts
@@ -0,0 +1,147 @@
+// import axiosInstance from "./axios";
+// import { API } from "./endpoints";
+
+// export interface Post {
+//   _id: string;
+//   user: {
+//     _id: string;
+//     username: string;
+//     fullName: string;
+//     profilePicture: string;
+//   };
+//   caption: string;
+//   imageUrl: string;
+//   likes: string[];
+//   likesCount: number;
+//   comments: {
+//     _id: string;
+//     user: {
+//       _id: string;
+//       username: string;
+//       profilePicture: string;
+//     };
+//     text: string;
+//     createdAt: Date;
+//   }[];
+//   commentsCount: number;
+//   isLiked: boolean;
+//   createdAt: Date;
+//   updatedAt: Date;
+// }
+
+// export const postAPI = {
+//   createPost: async (data: { caption: string; imageUrl: string }) => {
+//     const response = await axiosInstance.post(API.POSTS, data);
+//     return response.data;
+//   },
+
+//   getFeed: async (page: number = 1) => {
+//     const response = await axiosInstance.get(`${API.FEED}?page=${page}`);
+//     return response.data;
+//   },
+
+//   getUserPosts: async (userId: string) => {
+//     const response = await axiosInstance.get(API.USER_POSTS(userId));
+//     return response.data;
+//   },
+
+//   likePost: async (postId: string) => {
+//     const response = await axiosInstance.post(API.LIKE_POST(postId));
+//     return response.data;
+//   },
+
+//   addComment: async (postId: string, text: string) => {
+//     const response = await axiosInstance.post(API.COMMENT_POST(postId), { text });
+//     return response.data;
+//   },
+
+//   deletePost: async (postId: string) => {
+//     const response = await axiosInstance.delete(API.DELETE_POST(postId));
+//     return response.data;
+//   },
+// };
+
+
+
+import axiosInstance from "./axios";
+import { API } from "./endpoints";
+
+export interface Post {
+  _id: string;
+  user: {
+    _id: string;
+    username: string;
+    fullName: string;
+    profilePicture: string;
+  };
+  caption: string;
+  imageUrl: string;
+  likes: string[];
+  likesCount: number;
+  comments: {
+    _id: string;
+    user: {
+      _id: string;
+      username: string;
+      profilePicture: string;
+    };
+    text: string;
+    createdAt: Date;
+  }[];
+  commentsCount: number;
+  isLiked: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const postAPI = {
+  createPost: async (formData: FormData) => {
+    const response = await axiosInstance.post(API.POSTS, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  },
+
+  getFeed: async (page: number = 1) => {
+    const response = await axiosInstance.get(`${API.FEED}?page=${page}`);
+    return response.data;
+  },
+
+  getUserPosts: async (userId: string) => {
+    const response = await axiosInstance.get(API.USER_POSTS(userId));
+    return response.data;
+  },
+
+  likePost: async (postId: string) => {
+    const response = await axiosInstance.post(API.LIKE_POST(postId));
+    return response.data;
+  },
+
+  addComment: async (postId: string, text: string) => {
+    const response = await axiosInstance.post(API.COMMENT_POST(postId), { text });
+    return response.data;
+  },
+
+  deleteComment: async (postId: string, commentId: string) => {
+    const response = await axiosInstance.delete(
+      `/api/posts/${postId}/comment/${commentId}`
+    );
+    return response.data;
+  },
+
+  deletePost: async (postId: string) => {
+    const response = await axiosInstance.delete(API.DELETE_POST(postId));
+    return response.data;
+  },
+  getSavedPosts: async () => {
+    const response = await axiosInstance.get("/api/posts/saved");
+    return response.data;
+  },
+
+  getPostById: async (postId: string) => {
+    const response = await axiosInstance.get(`/api/posts/${postId}`);
+    return response.data;
+  },
+};
